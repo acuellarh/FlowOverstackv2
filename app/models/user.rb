ddb23f475e2c3b5358fa3a6fdc80bf06d5a9fd4b
Alejandro <acuellarh@gmail.com>
2019-06-10 21:04:11 -0500

    Method username created, this method extract the username of the email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,10 @@ class User < ApplicationRecord
 
   has_many :questions       
   has_many :answers       
-  has_many :comments       
+  has_many :comments   
+  
+  def username
+    self.email.split("@")[0]    
+  end
+  
 end
