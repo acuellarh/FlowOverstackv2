bad91553e660de585a15615db7df14871f4ead4a
Alejandro <acuellarh@gmail.com>
2019-07-06 12:00:48 -0500

    some general changes

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,58 @@
+class QuestionsController < ApplicationController
+  before_action :set_params, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.user = current_user
+
+    if @question.save
+      flash[:notice] = "Pregunta creada satisfactoriamente"
+      redirect_to questions_path
+    else
+      render :new
+    end    
+  end
+
+  def edit 
+     
+  end
+
+  def update    
+    if @question.update(question_params)
+      flash[:notice] = "Pregunta actualizada satisfactoriamente"  
+      redirect_to question_path(@question)
+    else
+      render :edit  
+    end
+  end 
+
+  def destroy
+    if @question.destroy
+      flash[:notice] = "Pregunta eliminada satisfactoriamente"  
+  
+      redirect_to questions_path
+    end
+  end  
+
+  private 
+
+  def question_params
+    params.require(:question).permit(:title, :content, :rating)  
+  end
+
+  def set_params
+    @question = Question.find(params[:id])  
+  end
+
+end
